abc2d1fe03123a91a05375b32d3161ca2dfc1b0f
Erick <0xEE33@proton.me>
2024-01-18 16:08:11 -0500

    Fetching accessToken

--- server/server.js
+++ server/server.js
@@ -1,6 +1,39 @@
 const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
+app.use(cors())
+
+let accessToken = null
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
+
+const getAccessToken = async () => {
+    try {
+        const response = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                grant_type: 'client_credentials',
+                client_id: clientId,
+                client_secret: clientSecret,
+            }),
+        })
+        const data = await response.json();
+        accessToken = data.access_token
+        console.log("Access Token:", accessToken)
+    } catch (error) {
+        console.error("Error fetching access token:", error)
+        throw error
+    }
+}
+
+getAccessToken()
 
 //Test Call
 app.get("/", (req, res) => {
